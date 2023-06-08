fdeaaaa946e0c622b4ebd71b69d2d279119d57cd
Thiri <thirihtet246@gmail.com>
2023-06-08 19:27:10 +0630

    Add user test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,7 @@ class User < ApplicationRecord
   after_create :save
 
   def recent_post
-    posts.order(updated_at: :asc).limit(3)
+    posts.order(updated_at: :desc).limit(3)
   end
 end
 
-# first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-# second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-
-# first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-# second_post = Post.create(author: first_user, title: 'Hello again', text: 'This is my second post')
-# third_post = Post.create(author: second_user, title: 'Hello', text: 'This is my first post')
-# fourth_post = Post.create(author: second_user, title: 'Hello again', text: 'This is my second post')
-# fifth_post = Post.create(author: first_user, title: 'Hello again', text: 'This is my third post')
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_151434) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  create_table "test_animals", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+    t.date "birth_date"
+    t.decimal "weight_kg"
+    t.string "species", limit: 50
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
