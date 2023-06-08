2119f475bcbb98919d7370f8815253946b9957a9
Thiri <thirihtet246@gmail.com>
2023-06-08 08:53:51 +0630

    delete unwanted comments

--- app/models/like.rb
+++ app/models/like.rb
@@ -8,8 +8,3 @@ class Like < ApplicationRecord
     post.update(likes_counter: post.likes.count)
   end
 end
-
-
-# Like.create(post: first_post, author: second_user).save
-# Like.create(post: first_post, author: first_user).save
-# Like.create(post: second_post, author: second_user).save
