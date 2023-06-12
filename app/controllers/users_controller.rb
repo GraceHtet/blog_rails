d804a0d37ebf9485622d79cb48484c966ebba89e
Thiri <thirihtet246@gmail.com>
2023-06-12 18:39:16 +0630

    Add controllers for users and posts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    return unless @user.nil?
+
+    render html: 'User not found', status: :not_found
+  end
+end
